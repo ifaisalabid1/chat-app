5667daf44c94de8055d8e38a38af4ff5c2af347e
Faisal Abid <ifaisalabid1@gmail.com>
2026-04-22 20:59:15 +0530

    Implement the read and write pump between hub and websocket

--- internal/delivery/ws/client.go
+++ internal/delivery/ws/client.go
@@ -1,6 +1,8 @@
 package ws
 
 import (
+	"context"
+	"log/slog"
 	"time"
 
 	"github.com/google/uuid"
@@ -20,4 +22,72 @@ type Client struct {
 	userID uuid.UUID
 	roomID uuid.UUID
 	send   chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error {
+		c.conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, text, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				slog.Error("websocket read error", "error", err)
+			}
+			break
+		}
+
+		if err := c.hub.PublishMessage(context.Background(), c.roomID, c.userID, string(text)); err != nil {
+			slog.Error("failed to publish message", "error", err)
+		}
+	}
+}
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+
+			if _, err := w.Write(msg); err != nil {
+				return
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
 }
